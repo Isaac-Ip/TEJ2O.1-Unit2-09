86ae1e45da8553b5dbab82a7df9677fddbe766a3
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-03 20:42:23 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,50 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Copyright (c) 2025 Isaac Ip All rights reserved
+Created by: Isaac Ip
+Created on: Oct 2025
+This program plays rock, paper, scissors.
 """
 
 from microbit import *
+import random
 
+score = 0
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    # shake the microbit
+    if accelerometer.was_gesture("shake"):
+        # chooses rock, paper or scissors
+        randomNumber = random.randint(0, 2)
+
+        if randomNumber == 0:
+            # rock
+            display.show(Image.SMALL_SQUARE)
+            sleep(5000)
+            display.show(Image.HAPPY)
+
+        if randomNumber == 1:
+            # paper
+            display.show(Image.SQUARE)
+            sleep(5000)
+            display.show(Image.HAPPY)
+
+        if randomNumber == 2:
+            # scissors
+            display.show(Image.SCISSORS)
+            sleep(5000)
+            display.show(Image.HAPPY)
+
+    # button A = add 1 to score
+    if button_a.was_pressed():
+        score += 1
+        sleep(5000)
+        display.show(Image.HAPPY)
+
+    # button B = show score
+    if button_b.was_pressed():
+        display.show(str(score))
+        sleep(5000)
+        display.show(Image.HAPPY)
