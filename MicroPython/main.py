254478ffd2f248f7ada978aff90b11aef2dbe6a6
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-07 17:26:20 +0000

    main.hex

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -10,41 +10,38 @@ import random
 
 score = 0
 
+# clear screen and show happy face
 display.clear()
 display.show(Image.HAPPY)
 
 while True:
-    # shake the microbit
+    # shake gesture = play rock, paper, scissors
     if accelerometer.was_gesture("shake"):
-        # chooses rock, paper or scissors
         randomNumber = random.randint(0, 2)
 
         if randomNumber == 0:
             # rock
             display.show(Image.SMALL_SQUARE)
-            sleep(5000)
-            display.show(Image.HAPPY)
-
         if randomNumber == 1:
             # paper
             display.show(Image.SQUARE)
-            sleep(5000)
-            display.show(Image.HAPPY)
-
         if randomNumber == 2:
             # scissors
             display.show(Image.SCISSORS)
-            sleep(5000)
-            display.show(Image.HAPPY)
 
-    # button A = add 1 to score
+        sleep(5000)
+        display.show(Image.HAPPY)
+
+    # button A = add 1 to score and show check mark
     if button_a.was_pressed():
         score += 1
+        display.show(Image.YES)
         sleep(5000)
         display.show(Image.HAPPY)
 
     # button B = show score
     if button_b.was_pressed():
+        display.scroll("Score: ")
         display.show(str(score))
         sleep(5000)
         display.show(Image.HAPPY)
